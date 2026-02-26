78a8ea59f97ce7f03a7a72e5bd957cf8a2986f04
Josh Montoya <josh@montoya.io>
2026-02-26 14:24:50 -0800

    move all errors to streambuf.go

--- streambuf.go
+++ streambuf.go
@@ -2,7 +2,23 @@
 // can block until more data is written or the buffer is closed.
 package streambuf
 
-import "context"
+import (
+	"context"
+	"errors"
+)
+
+var (
+	// ErrSeekEndNotSupported is returned when seeking relative to the end.
+	// Reader-backed seeks currently support only SeekStart and SeekCurrent.
+	ErrSeekEndNotSupported = errors.New("seek end is not currently supported")
+	// ErrInvalidWhence is returned when Seek receives an unsupported whence value.
+	ErrInvalidWhence = errors.New("invalid seek whence")
+	// ErrNegativeIndex is returned when a seek would move before byte index 0.
+	// The reader position is clamped to 0 in this case.
+	ErrNegativeIndex = errors.New("invalid index, cannot be less than 0")
+	// ErrIsClosed is returned when an action is attempted on a closed instance.
+	ErrIsClosed = errors.New("cannot perform action on closed instance")
+)
 
 var expiredContext context.Context
 
