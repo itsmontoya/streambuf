1467dd40ad6f8732235b19e4fbe42c33f0c2ac5f
Josh Montoya <josh@montoya.io>
2026-02-26 12:17:53 -0800

    implement tests for NewReadOnly

--- buffer_test.go
+++ buffer_test.go
@@ -49,3 +49,51 @@ func Test_New_invalid_filepath(t *testing.T) {
 		})
 	}
 }
+
+func Test_NewReadOnly_invalid_filepath(t *testing.T) {
+	type testcase struct {
+		name string // description of this test case
+
+		filepath string
+
+		wantErr bool
+	}
+
+	tests := []testcase{
+		{
+			name:     "missing file",
+			filepath: t.TempDir() + "/missing-file.tmp",
+			wantErr:  true,
+		},
+		{
+			name:     "empty filepath",
+			filepath: "",
+			wantErr:  true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var (
+				b   *Buffer
+				err error
+			)
+
+			if b, err = NewReadOnly(tt.filepath); err != nil {
+				if !tt.wantErr {
+					t.Fatalf("NewReadOnly() unexpected error: %v", err)
+				}
+
+				return
+			}
+
+			if tt.wantErr {
+				t.Fatal("NewReadOnly() expected error, received <nil>")
+			}
+
+			t.Cleanup(func() {
+				_ = b.Close()
+			})
+		})
+	}
+}
