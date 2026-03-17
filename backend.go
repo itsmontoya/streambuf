16f17a9287d8d08f36b58a0eafccfba346e727b1
Josh Montoya <josh@montoya.io>
2026-03-17 12:48:04 -0700

    remove no longer needed files

--- backend.go
+++
@@ -1,8 +0,0 @@
-package streambuf
-
-type backend interface {
-	Write(bs []byte) (n int, err error)
-	ReadAt(in []byte, index int64) (n int, err error)
-	CloseWriter() (err error)
-	CloseReader() (err error)
-}
