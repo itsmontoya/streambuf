564dc65059bbefd10f4d2ac056b5e58e5557ad86
Josh Montoya <josh@montoya.io>
2026-03-17 12:34:50 -0700

    add stream examples

--- streambuf_test.go
+++ streambuf_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 var exampleBuffer *Buffer
+var exampleStream *Stream
 
 func ExampleNew() {
 	var err error
@@ -15,23 +16,16 @@ func ExampleNew() {
 	}
 }
 
-func ExampleNewReadOnly() {
+func ExampleNewStream() {
 	var err error
-	// NewReadOnly constructs a read-only file-backed buffer.
-	// Calls to Write on this buffer return ErrCannotWriteToReadOnly.
-	if exampleBuffer, err = NewReadOnly("path/to/file"); err != nil {
+	// NewStream constructs a read-only file-backed stream.
+	if exampleStream, err = NewStream("path/to/file"); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func ExampleNewMemory() {
 	exampleBuffer = NewMemory()
-}
-
-func ExampleNewReadOnlyMemory() {
-	// NewReadOnlyMemory constructs a read-only memory-backed buffer.
-	// Calls to Write on this buffer return ErrCannotWriteToReadOnly.
-	exampleBuffer = NewReadOnlyMemory([]byte("hello world"))
 }
 
 func ExampleBuffer_Write() {
@@ -82,6 +76,48 @@ func ExampleBuffer_CloseAndWait() {
 	// CloseAndWait blocks until the backend is closed and all readers are closed,
 	// or until the provided context is done.
 	if err := exampleBuffer.CloseAndWait(context.Background()); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func ExampleStream_Reader() {
+	var (
+		r1  io.ReadSeekCloser
+		r2  io.ReadSeekCloser
+		r3  io.ReadSeekCloser
+		err error
+	)
+
+	if r1, err = exampleStream.Reader(); err != nil {
+		log.Fatal(err)
+	}
+	defer r1.Close()
+
+	if r2, err = exampleStream.Reader(); err != nil {
+		log.Fatal(err)
+	}
+	defer r2.Close()
+
+	if r3, err = exampleStream.Reader(); err != nil {
+		log.Fatal(err)
+	}
+	defer r3.Close()
+
+	// Each reader is independent and maintains its own read offset.
+	// Reads or seeks on r1 do not affect r2 or r3.
+}
+
+func ExampleStream_Close() {
+	// Close closes the readable backend immediately and does not wait for readers.
+	if err := exampleStream.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func ExampleStream_CloseAndWait() {
+	// CloseAndWait blocks until the readable backend is closed and all readers are
+	// closed, or until the provided context is done.
+	if err := exampleStream.CloseAndWait(context.Background()); err != nil {
 		log.Fatal(err)
 	}
 }
